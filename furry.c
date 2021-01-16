02b24c2de28bd214a47e24b913f2dbd3fb0682a1
blares3000 <77177640+blares3000@users.noreply.github.com>
2021-01-16 12:01:05 -0800

    furry.c

---
+++ furry.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi there, I am the head of a furry\n");
+    printf(" |\\                           /|\n");
+    printf(" | \\                         / |\n");
+    printf(" |  \\                       /  |\n");
+    printf(" ||\\ \\                   / /||\n");
+    printf(" || \\ \\                 / / ||\n");
+    printf(" ||  \\ \\               / /  ||\n");
+    printf(" ||   \\ \\             / /   ||\n");
+    printf(" ||    \\ \\           / /    ||\n");
+    printf("|-----------------------------|\n");
+    printf("|                             |\n");
+    printf("|                             |\n");
+    printf("|         ()       ()         |\n");
+    printf("|                             |\n");
+    printf("|       \\_____________/      |\n");
+    printf("|                             |\n");
+    printf("|                             |\n");
+    printf("|                             |\n");
+    printf("|-----------------------------|  ");
+
+
+
+    return 0;
+}
